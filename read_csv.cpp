b85c8091c069c6fc935fdc394a25364a469c87d7
Shaked Pollak <shaked.pollak05@gmail.com>
2025-07-28 14:36:38 +0300

    Final

--- read_csv.cpp
+++ read_csv.cpp
@@ -3,66 +3,67 @@
 #include <fstream> // file operation
 #include <string> // use strings
 #include <vector> // store variables to heap (dynamic allocation)
-#include <sstream> // lets
+#include <sstream> //
 #include <cctype> // to use isdigit
+#include <ranges>
 
 using namespace std;
 
-bool checkString(const string& str) { // copy of string, make sure that the function won't change str
-    if (str.empty()) {
+
+bool checkString(const string& str, double& value) { // copy of string, make sure that the function won't change str
+    try {
+        size_t idx;
+        value = stod(str, &idx); // init idx to the last char that is a number (or 0 if starts with char that is not a number)
+        // value is passed by reference so it will be changed to the number until the first char in the string that is not a number
+        // for example, after - value = stod("45A", &idx), value = 45, idx = 2.
+        return idx == str.size(); // returns true if all the string is a number
+    }
+    catch (invalid_argument&) {
         return false;
     }
-    if (str[0] == '-') {
-        if (str.size() == 1) {
-            return false;
-        }
+    catch (out_of_range&) {
+        return false;
     }
-
-    int countPoints = 0;
-    for (int i = 1 ; i < str.size() ; i++) {
-        // if (str[i] == '.') {
-        //     continue;
-        //     countPoints++;
-        // }
-        // if (!isdigit(str[i])) {
-        //     return false;
-        // }
-        // check if digits and doesnt have more than one decimal point
-    }
-    return true;
 }
 
 int main() {
     string filePath;
-    // cout << "Enter CSV file path" << endl;
-    // getline(cin, filePath); // allows spaces in file path
+
+    cout << "Enter CSV file path" << endl;
+    getline(cin, filePath); // allows spaces in file path
 
     fstream file; // define file stream object
-    // file.open(filePath);
-    file.open("C:\\Users\\hanoi\\Desktop\\maman11\\example_no_bom.csv");
+    file.open(filePath);
+
     if (!file.is_open()) {
         cout << "File did not open correctly" << endl;
         return -1;
     }
     string line;
-    vector<vector<double>> array; // init 2D array
+    vector<vector<double>> array; // init 2D array of doubles
 
     int row = 0;
-
-    while (getline(file, line)) { // copies line in pFile to the string variable named "line"
-        array.push_back(vector<double>());
+    int maxColumn = 0;
+    while (getline(file, line)) { // copies line in file to the string variable named "line"
+        vector<double> currRow;
         double rowSum = 0;
-        stringstream ss(line); // this way, we can treat "line" as a file
+        stringstream ss(line); // this way, we can treat "line" as a file and access each cell
         string cell;
         int col = 0;
         while (getline(ss, cell, ',')) {
-            if (checkString(cell)) {
-                double num = stoi(cell);
-                cout << num << endl;
-                array[row].push_back(num);
+            double num = 0;
+            if (checkString(cell, num)) {
                 rowSum += num;
-                col++;
+                currRow.push_back(num);
             }
+            else { // if string not a valid number, push 0.
+                currRow.push_back(0);
+            }
+            col++;
+        }
+        array.push_back(currRow);
+        if (col > maxColumn) {
+            maxColumn = col;
         }
         cout << "Total of line: " << rowSum << endl;
         row++;
@@ -71,11 +72,14 @@ int main() {
 
     cout << "Sums of columns:" << endl;
     vector<double> allColumnsSums;
-    for (int i = 0 ; i < array.size() ; i++) {
+    for (int col = 0; col < maxColumn ; col++) {
         double columnSum = 0;
-        for (int j = 0 ; j < array[i].size() ; j++) {
-            columnSum += array[i][j];
+
+        for (int row = 0; row < array.size(); row++) {
+            if (array[row].size() > col) {
+                columnSum += array[row][col];
+            }
         }
-        cout << columnSum;
+        cout << columnSum << " ";
     }
 }
